3da1b99f714d32abedf5dbab870e72ba1cb44a4b
PeterMa <jma4@ualberta.ca>
2021-01-25 13:38:20 -0700

    remove unused routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import HomePage from "./pages/HomePage";
 
 
 Vue.use(VueRouter);
 
 const routes = [
-    {path: '/home', component: HomePage, name: 'home', meta: {title: 'Home - ERP System'}},
+
 ];
 
 const router = new VueRouter({
