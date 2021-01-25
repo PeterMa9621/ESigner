bf6304d83a9ca45395f0e7dcca4e5613aa7dd3c1
PeterMa <jma4@ualberta.ca>
2021-01-25 11:39:23 -0700

    add some tests

---
+++ tests/Feature/RoutesTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RoutesTest extends TestCase
+{
+    /**
+     * Test home page.
+     *
+     * @return void
+     */
+    public function testHomePageTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test home page.
+     *
+     * @return void
+     */
+    public function testSignerPageTest()
+    {
+        $response = $this->get('/signer');
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test home page.
+     *
+     * @return void
+     */
+    public function testUploaderPageTest()
+    {
+        $response = $this->get('/uploader');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test home page.
+     *
+     * @return void
+     */
+    public function testApiDocumentTest()
+    {
+        $response = $this->get('/api/documents');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test home page.
+     *
+     * @return void
+     */
+    public function testApiSignaturePositionTest()
+    {
+        $response = $this->get('/api/signature_positions');
+
+        $response->assertStatus(405);
+    }
+}
